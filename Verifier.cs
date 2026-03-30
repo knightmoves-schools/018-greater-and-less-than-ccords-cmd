7522a74b3eb4fbccd07f1c8964b6c7f00a58fef5
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 13:03:08 -0500

    Format Verifier.cs with consistent syntax

--- Verifier.cs
+++ Verifier.cs
@@ -6,23 +6,23 @@ public string OldEnough(int age){
         return "old enough";
     }
         return "";
-};
+}
 public string UnderAge(int age){
     if (age < 18){
         return "under age";
     }
         return "";
-};
+}
 public string LegalSpeed(int speed){
     if (speed <= 50){
         return "legal speed";
     }
         return "";
-};
+}
 public string IsGoldenYears(int age){
     if (age >= 65){
         return "golden years";
     }
         return "";
-};
+}
 }
