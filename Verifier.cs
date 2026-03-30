51539e6b576db7d5333dd0ba1dab70855c6008b8
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 12:58:55 -0500

    Update Verifier.cs

--- Verifier.cs
+++ Verifier.cs
@@ -1,5 +1,32 @@
 ﻿namespace knightmoves;
 public class Verifier
 {
-    // Add your code here
+public string OldEnough(int age){
+    if (age > 17){
+        return "old enough";
+    }else{
+        return "";
+    }
+};
+public string UnderAge(int age){
+    if (age < 18){
+        return "under age";
+    }else{
+        return "";
+    }
+};
+public string LegalSpeed(int speed){
+    if (speed < 50){
+        return "legal speed";
+    }else{
+        return "";
+    }
+};
+public string IsGoldenYears(int age){
+    if (age > 65){
+        return "golden years";
+    }else{
+        return "";
+    }
+};
 }
